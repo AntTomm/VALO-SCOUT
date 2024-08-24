8e4b8eaca15f995181f79e122137c1827d17d57d
Anthony Tommaso <atommaso05@gmail.com>
2024-08-24 00:19:05 -0400

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"; // vercel
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "VALO-Scout",
-  description: "Wanna learn about the TOP VALORANT players? Experiment with VALO-Scout now!",
+  description: "Wanna learn about the TOP VALORANT players AND get tips on who to play? Experiment with VALO-Scout now!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics /> 
+      </body>
     </html>
   );
 }
